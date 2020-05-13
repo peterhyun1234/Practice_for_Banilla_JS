f8229242bf6da096eb59c1a04e805093c0253109
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-13 20:51:53 +0900

    color change by using event hadler

--- Chrome_app_1/event.js
+++ Chrome_app_1/event.js
@@ -1,19 +1,34 @@
 const title = document.querySelector("#title");
 
-//console.dir(title); 
+const BASE_COLOR = "blue";
+const OTHER_COLOR = "red";
+
+//console.dir(title);
+/* 
 function handleResize() {
     console.log("I have been resized");
-};
+};*/
 
 // resize 이벤트가 오는 것을 기다림!
 //window.addEventListener("resize", handleResize()); // handleResize()라고 넣으면 바로 함수가 호출됨
-window.addEventListener("resize", handleResize); // resize되면 호출
+//window.addEventListener("resize", handleResize); // resize되면 호출
 
 
 //console.dir(title); 
+
 function handleClick() {
-    console.log("Title have been Clicked");
-    title.style.color = "blue";
+    const currentColor = title.style.color;
+    console.log("Title have been Clicked: " + currentColor);
+    if (currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 };
 
-title.addEventListener("click", handleClick); // click되면 호출
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick); // click되면 호출
+}
+
+init();
