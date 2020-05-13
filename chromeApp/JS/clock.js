ea0d2a25af368bdb20477e8ded90caa22f69c87d
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-13 22:05:52 +0900

    add simple form

--- chromeApp/JS/clock.js
+++ chromeApp/JS/clock.js
@@ -11,7 +11,7 @@ function getTime() {
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`; // 10초보다 작으면 0을 넣는 삼항연산자
 
 
 }
